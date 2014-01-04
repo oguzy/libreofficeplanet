9f76be7fff9645995c1fb3ee6d1a7f30c3fdb191
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2014-01-04 21:46:40 +0200

    working release

---
+++ libreoffice/output/a11y-ruler.js
@@ -0,0 +1,26 @@
+document.write('<style type=\"text/css\">');
+document.write('body { margin: 0; } #gnome-fog-ruler { background: #57AD06; padding: 16px; color: #fff; font-family: "Lucida Grande", "DejaVu Sans", "Bitstream Vera Sans", Verdana, sans-serif; font-size: 12px; z-index: 200; position: relative; margin-bottom: 0px;}');
+document.write('#gnome-fog-ruler * { margin: 0; padding: 0; }');
+document.write('#gnome-fog-ruler .fog-center { width: 960px; margin: 0 auto; text-align: left; }');
+document.write('#gnome-fog-ruler a { color: #fff; }');
+document.write('#gnome-fog-ruler .fog-intro { width: 380px; float: left; margin-right: 12px; }');
+document.write('#gnome-fog-ruler .fog-intro h4 { margin-bottom: 6px; color: #fff; font-size: 13px; text-shadow: 0 1px 2px #346704; }');
+document.write('#gnome-fog-ruler .fog-ruler { background: #fff; overflow: hidden; -moz-border-radius: 4px; -webkit-border-radius: 4px; padding: 2px; -moz-box-shadow: 0 1px 2px #346704; -webkit-box-shadow: 0 1px 2px #346704; }');
+document.write('#gnome-fog-ruler .fog-ruler .fog-bar { display: block; float: left; font-size: 5px; height: 14px; line-height: 12px; }');
+document.write('#gnome-fog-ruler .green { background: #66bb14; }');
+document.write('#gnome-fog-ruler .orange { background: #f57900; }');
+document.write('#gnome-fog-ruler .blue { background: #729fcf; }');
+document.write('#gnome-fog-ruler .yellow { background: #fce94f; }');
+document.write('#gnome-fog-ruler .white { background: #fff; }');
+document.write('#gnome-fog-ruler .green { background: #8ae234; }');
+document.write('#gnome-fog-ruler .fog-description { margin-top: 8px; overflow: hidden; text-align: center; }');
+document.write('#gnome-fog-ruler .fog-description .fog-item { margin-right: 12px; display: block; float: left; overflow: hidden; text-align: left; }');
+document.write('#gnome-fog-ruler .fog-description .fog-item .fog-box { display: block; float: left; width: 12px; height: 12px; border: 1px solid #fff; margin: 0 6px 6px 0; -moz-border-radius: 3px; -webkit-border-radius: 3px; border-radius: 3px; -moz-box-shadow: 0 1px 2px #346704; -webkit-box-shadow: 0 1px 2px #346704; }');
+document.write('#gnome-fog-ruler .fog-description .fog-item .fog-details { font-style: italic; display: block; margin-left: 20px; }');
+document.write('#gnome-fog-ruler .fog-donate { float: right; margin-top: 11px; }');
+document.write('#gnome-fog-ruler .fog-donate a { display: block; padding: 6px 8px; background: #fff; color: #333; font-weight: bold; text-decoration: none; font-size: 14px; -moz-box-shadow: 0 1px 2px #346704, inset 0 -3px 10px #ccc; -webkit-box-shadow: 0 1px 2px #346704, inset 0 -3px 10px #ccc; -moz-border-radius: 4px; -webkit-border-radius: 4px; }');
+document.write('#gnome-fog-ruler .fog-donate a:hover { color: #204a87; text-decoration: underline; }');
+document.write('</style>');
+document.write('<div id=\"gnome-fog-ruler\">Thank you all donors who donated in total $20,000 to our <a href="http://www.gnome.org/friends">Friends of GNOME program</a>, helping us to make 2012 the year of accessibility!</div>');
+
+
